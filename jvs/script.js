f744efd804da239555657e03d89345e9fb6e9775
michelangelomancini <michelangelomancini8@gmail.com>
2025-01-13 16:53:07 +0100

    consegna milestone 1

--- jvs/script.js
+++ jvs/script.js
@@ -1,36 +1,52 @@
-// INPUT- come input chiediamo all'utente (passeggero) di inserire il numero di chilometri che vuole percorrere e l'età.
-let risultato ;
+// inserimento variabili di input 
+
 // input chilometri
-let chilometriUtente = parseInt(prompt("inserisci il numero di chilometri da fare (un numero)"));
+const chilometriUtente = document.getElementById('kilometers');
 
 // input età
-let etaUtente = parseInt(prompt("inserisci una età (un numero)"));
+const etaUtente =document.getElementById('age');
 
-console.log(chilometriUtente, etaUtente);
+// richiamo button
+let button = document.getElementById('calcbutton');
 
-// ELABORAZIONE 
-// calcolare il prezzo del biglietto in base ai km (0.21 € al km)
+// aggiunta event al click del button
 
- 
+button.addEventListener('click',()=> {
+    const chilometri = (chilometriUtente.value);
+    const eta = (etaUtente.value);
+    console.log("I km da perccorrere sono:",chilometri,"-","La tua età è di anni:", eta);
+    
+    let risultato ;
+    
  // SE l'utente ha <18 anni (minorenne)
- if (etaUtente < 18){
-// ALLORA applicare sconto del 20%
- // ELABORAZIONE moltiplicare il numero dei chilometri per 0.21 e togliere un quinto dal risultato
- let risultato = chilometriUtente * 0.168 ;
+ if (eta < 18){
+    // ALLORA applicare sconto del 20%
+     
+     let risultato = chilometri * 0.168 ;
+    
+     console.log("Complimenti hai diritto allo sconto, pagherai:", risultato.toFixed( 2 ), "€" );
+    }
+     // SE l'utente ha >65 anni (anziano)
+     else if (eta >65) {
+     // ALLORA applicare sconto del 40%
+     
+     let risultato = chilometri * 0.126 ;
+    
+     console.log("Complimenti hai diritto allo sconto, pagherai:", risultato.toFixed( 2 ), "€"  );
+    }
+    // SE l'utente ha >18 <65 anni (adulto)
+    else {
+         // ELABORAZIONE moltiplicare il numero dei chilometri per 0.21
+        let risultato = (chilometri * 0.21)
+        console.log("Pagherai:", risultato.toFixed( 2 ), "€"  );
+    }
 
- console.log("Complimenti hai diritto allo sconto, pagherai:", risultato.toFixed( 2 ), "€" );
-}
- // SE l'utente ha >65 anni (anziano)
- else if (etaUtente >65) {
- // ALLORA applicare sconto del 40%
- // ELABORAZIONE moltiplicare il numero dei chilometri per 0.21 e togliere due quinti dal risultato
- let risultato = chilometriUtente * 0.126 ;
 
- console.log("Complimenti hai diritto allo sconto, pagherai:", risultato.toFixed( 2 ), "€"  );
-}
-// SE l'utente ha >18 <65 anni (adulto)
-else {
-     // ELABORAZIONE moltiplicare il numero dei chilometri per 0.21
-    let risultato = (chilometriUtente * 0.21)
-    console.log("Pagherai:", risultato.toFixed( 2 ), "€"  );
-}
+
+
+
+});
+// funzione di default
+button.addEventListener ('submit', function(event) {
+    event.preventDefault()
+});
